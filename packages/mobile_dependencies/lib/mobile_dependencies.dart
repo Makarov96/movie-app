6a72e392d1310f80a5b9a358d0fa4c1733dc397c
Makarov96 <estev97@gmail.com>
2024-03-21 12:07:48 -0600

    feat: export equatable

--- packages/mobile_dependencies/lib/mobile_dependencies.dart
+++ packages/mobile_dependencies/lib/mobile_dependencies.dart
@@ -2,5 +2,6 @@ export 'package:flutter_riverpod/flutter_riverpod.dart';
 export 'package:go_router/go_router.dart';
 export 'package:intl/intl.dart';
 export 'package:dio/dio.dart';
+export 'package:equatable/equatable.dart';
 export 'package:kueski_http/kueski_http.dart';
 export 'package:flutter_localizations/flutter_localizations.dart';
